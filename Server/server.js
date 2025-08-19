2289537222f2ae75bae6f909e5feceeb8be4465f
Arti <dwivediarti214@gmail.com>
2025-08-19 23:11:52 +0530

    Modify last Time

--- Server/server.js
+++ Server/server.js
@@ -1,38 +1,27 @@
-import express from "express";
-import cors from "cors";
-import "dotenv/config";
-import cookieParser from "cookie-parser";
-import connectDb from "./config/mongoConnect.js";
-import authRouter from "./routes/authRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import cookieParser from 'cookie-parser';
+import connectDb from './config/mongoConnect.js';
+import authRouter from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
-const app = express();
 
-// DB connect
+const app=express();
+const port=process.env.PORT || 4000;
 connectDb();
-
-// CORS setup (include deployed frontend URL later)
-const allowedOrigins = [
-  "http://localhost:5173", 
-  "https://mern-authentication-client-sigma.vercel.app/"
-];
-
+const allowedOrigins=['http://localhost:5173']
 app.use(express.json());
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  })
-);
+app.use(cors({
+    origin:allowedOrigins,
+    credentials:true
+}));
 app.use(cookieParser());
-
-// Routes
-app.use("/api/auth", authRouter);
-app.use("/api/user", userRoutes);
-
-app.get("/", (req, res) => {
-  res.send("API is working 🚀");
+app.use('/api/auth',authRouter);
+app.use('/api/user',userRoutes);
+app.get('/',(req,res)=>{
+    res.send("Api is working");
+})
+app.listen(port,()=>{
+    console.log(`Server started on ${port}`);
 });
-
-
-export default app;
